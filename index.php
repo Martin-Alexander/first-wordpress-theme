c871e6cca975b065b63c9c1b696c3f3787508e76
Martin-Alexander <martingianna@gmail.com>
2018-07-22 19:00:55 -0400

    basic css configuration

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+  $template_dir = get_bloginfo('template_directory');
+  $css_dir = "{$template_dir}/style.css"
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Page Title</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="<?php echo($css_dir) ?>">
+</head>
+<body>
+  <h1>Hello World</h1>
+</body>
+</html>
